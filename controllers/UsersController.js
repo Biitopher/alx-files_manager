00a167f3ddb2a035f2b2552851a0f6e2958630c0
Evans Sese <sese_evans@yahoo.com>
2024-03-11 13:00:56 +0300

    [Feature] Task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -39,6 +40,29 @@ class UsersController {
     };
 
     return res.status(201).json(createdUser);
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.usersCollection.findOne({ _id: userId });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userInfo = {
+      id: user._id,
+      email: user.email,
+    };
+    return res.status(200).json(userInfo);
   }
 }
 
