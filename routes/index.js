6dc242b02856f5074e62751971bda5cd77a7caa8
Evans Sese <sese_evans@yahoo.com>
2024-03-11 19:09:22 +0300

    [Feature] Task 7

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,6 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unplublish', FilesController.putUnpublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 export default router;
