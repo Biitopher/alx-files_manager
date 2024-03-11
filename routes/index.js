1d088148f801e7a28b02868c39f048b3f8a07ed2
Evans Sese <sese_evans@yahoo.com>
2024-03-11 17:03:27 +0300

    [Feature] Task 6

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,7 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 export default router;
