4ac27d44714a231939d5b0eaaac8f16e24a770cb
Evans Sese <sese_evans@yahoo.com>
2024-03-12 12:44:05 +0300

    [Feature] Added test suite

---
+++ tests/redis.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+import { expect, use, should, before, after } from 'chai';
+import chaiHttp from 'chai-http';
+import { promisify } from 'util';
+
+import redisClient from '../utils/redis';
+
+use(chaiHttp);
+should();
+
+// redisClient
+describe('redis Client', () => {
+  before(async () => {
+    await redisClient.client.flushall('ASYNC');
+  });
+
+  after(async () => {
+    await redisClient.client.flushall('ASYNC');
+  });
+
+  it('shows that connection is alive', async () => {
+    expect(redisClient.isAlive()).to.equal(true);
+  });
+
+  it('returns key as null because it does not exist', async () => {
+    expect(await redisClient.get('myKey')).to.equal(null);
+  });
+
+  it('set key can be called without issue', async () => {
+    expect(await redisClient.set('myKey', 12, 1)).to.equal(undefined);
+  });
+
+  it('returns key with null because it expired', async () => {
+    const sleep = promisify(setTimeout);
+    await sleep(1100);
+    expect(await redisClient.get('myKey')).to.equal(null);
+  });
+});
